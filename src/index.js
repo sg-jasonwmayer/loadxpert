4d37341043efc73064411aec7e32d600d222b4d0
Jason Mayer <flavorflavor@gmail.com>
2020-10-19 16:33:52 -0400

    sass update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import Header from  './components/Header';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Header />
-      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
